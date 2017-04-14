47249d8b9701d8389f6c20d7eb55ac5401f9d390
John DeNero <denero@berkeley.edu>
2017-04-13 20:46:45 -0700

    runs, but has a graph bug: inputs from different frames

--- seq2seq/decoders/peeking_decoder.py
+++ seq2seq/decoders/peeking_decoder.py
@@ -8,58 +8,41 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import tensorflow as tf
-import numpy as np
 from seq2seq.decoders.attention_decoder import AttentionDecoder, AttentionDecoderOutput
 
-from seq2seq.contrib.seq2seq.helper import CustomHelper
-
-from tensorflow.python.framework import function  # pylint: disable=E0611
-
 from keras.layers.core import Activation, Dense, RepeatVector
-from keras.layers.merge import Concatenate
+from keras.layers.merge import concatenate
 from keras.layers.wrappers import TimeDistributed
 
 class PeekingDecoder(AttentionDecoder):
-  """An RNN Decoder that uses attention over an input sequence.
-
-  Args:
-    cell: An instance of ` tf.contrib.rnn.RNNCell`
-    helper: An instance of `tf.contrib.seq2seq.Helper` to assist decoding
-    initial_state: A tensor or tuple of tensors used as the initial cell
-      state.
-    vocab_size: Output vocabulary size, i.e. number of units
-      in the softmax layer
-    attention_keys: The sequence used to calculate attention scores.
-      A tensor of shape `[B, T, ...]`.
-    attention_values: The sequence to attend over.
-      A tensor of shape `[B, T, input_dim]`.
-    attention_values_length: Sequence length of the attention values.
-      An int32 Tensor of shape `[B]`.
-    attention_fn: The attention function to use. This function map from
-      `(state, inputs)` to `(attention_scores, attention_context)`.
-      For an example, see `seq2seq.decoder.attention.AttentionLayer`.
-    reverse_scores: Optional, an array of sequence length. If set,
-      reverse the attention scores in the output. This is used for when
-      a reversed source sequence is fed as an input but you want to
-      return the scores in non-reversed order.
-  """
+  """An RNN Decoder that uses attention over an input sequence (and peeks)."""
 
   def __init__(self, *args, **vargs):
     super().__init__(*args, **vargs)
-    self.attention_values_dim = self.attention_values.shape.dims[-1].value
-    self.softmax_input_layer = Dense(self.cell.output_size, activation='tanh')
-    self.logit_layer = Dense(self.vocab_size)
+
+  @staticmethod
+  def one_per_batch(params, indices):
+    """Return one slice of params per batch, selected by ints in indices.
+
+    params: batched values
+      A tensor of shape [B, ...]
+    indices: an index into the 2nd dimension of params
+      A tensor of shape [B]
+    """
+    batch_size = indices.shape[0].value
+    batched_indices = tf.transpose(tf.stack([tf.range(batch_size, dtype=indices.dtype), indices]))
+    return tf.gather_nd(params, batched_indices)
 
   def compute_vocab_probs_for_attentions(self, cell_output):
     """Computes the decoder output probabilities under each possible attention."""
 
     # Generate softmax inputs for each possible attention
-    cell_outputs = RepeatVector(self.attention_values_length)(cell_output)
-    softmax_inputs = TimeDistributed(self.softmax_input_layer)(
-      Concatenate([cell_outputs, self.attention_values], axis=-1))
+    cell_outputs = RepeatVector(self.max_sentence_length)(cell_output)
+    merged = concatenate([cell_outputs, self.attention_values])
+    softmax_inputs = TimeDistributed(self.softmax_input_layer)(merged)
 
     # Softmax computation for each possible attention
-    return TimeDistributed(Activation('softmax')(self.logit_layer))(softmax_inputs)
+    return Activation('softmax')(TimeDistributed(self.logit_layer)(softmax_inputs))
 
   def choose_attentions(self, vocab_probs, correct_words):
     """Compute the best attention index.
@@ -69,26 +52,37 @@ class PeekingDecoder(AttentionDecoder):
     correct_words: Indices of the correct vocabulary choices
       A tensor of shape [B]
     """
-    correct_probs = tf.gather_nd(tf.transpose(vocab_probs, [0, 2, 1]), correct_words)
+    correct_probs = self.one_per_batch(tf.transpose(vocab_probs, [0, 2, 1]), correct_words)
     return tf.argmax(correct_probs, axis=-1)
 
   def compute_output(self, cell_output, attentions):
     """Computes the decoder outputs."""
-
     att_scores = tf.one_hot(attentions, self.attention_values_dim)
-    attention_context = tf.gather_nd(self.attention_values, attentions)
-    softmax_input = self.softmax_input_layer(Concatenate([cell_output, attention_context]))
+    batch_size = cell_output.shape[0].value
+    attention_context = self.one_per_batch(self.attention_values, attentions)
+    softmax_input = self.softmax_input_layer(concatenate([cell_output, attention_context]))
     logits = self.logit_layer(softmax_input)
 
     return softmax_input, logits, att_scores, attention_context
 
   def step(self, time_, inputs, state, name=None):
     cell_output, cell_state = self.cell(inputs, state)
+
+    # setup
+    self.max_sentence_length = self.attention_values.shape[-2].value
+    self.attention_values_dim = self.attention_values.shape[-1].value
+    merged_dim = self.attention_values_dim + cell_output.shape[-1].value
+    self.softmax_input_layer = Dense(self.cell.output_size, activation='tanh', input_shape=(merged_dim,))
+    self.logit_layer = Dense(self.vocab_size, input_shape=(self.cell.output_size,))
+
+    # find best attentions
     vocab_probs = self.compute_vocab_probs_for_attentions(cell_output)
 
-    # TODO Replace with actual correct words, encoded as one vocabulary index per position
-    correct_words = tf.ones(shape=(4, 16))
+    # TODO Replace with indices of actual correct words, encoded as one vocabulary index per position
+    batch_size = cell_output.shape.dims[0].value
+    correct_words = tf.ones(shape=(batch_size,), dtype=tf.int32)
 
+    # compute output
     attentions = self.choose_attentions(vocab_probs, correct_words)
     cell_output_new, logits, attention_scores, attention_context = \
       self.compute_output(cell_output, attentions)
